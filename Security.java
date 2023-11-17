f876a1cb25188b071e255be97d237ecb72ad654d
chuenyen <54228694+chuenyen@users.noreply.github.com>
2023-11-17 22:43:53 +0800

    Push to remote (#7)
    * add

---
+++ Security.java
@@ -0,0 +1,150 @@
+package com.example.model;
+
+public class Security {
+
+	private String companyName;
+	private String isin;
+	private String targetPrice;
+	private String stockRating;
+	private String stockRatingWM;
+	private String stockRatingDate;
+	
+	private String highlight;
+	private String investmentRisk;
+	private String businessSummary;
+	private String subIndustryOutlook;
+	private String analystNote;
+	/**
+	 * @return the companyName
+	 */
+	public String getCompanyName() {
+		return companyName;
+	}
+	/**
+	 * @param companyName the companyName to set
+	 */
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	/**
+	 * @return the isin
+	 */
+	public String getIsin() {
+		return isin;
+	}
+	/**
+	 * @param isin the isin to set
+	 */
+	public void setIsin(String isin) {
+		this.isin = isin;
+	}
+	/**
+	 * @return the targetPrice
+	 */
+	public String getTargetPrice() {
+		return targetPrice;
+	}
+	/**
+	 * @param targetPrice the targetPrice to set
+	 */
+	public void setTargetPrice(String targetPrice) {
+		this.targetPrice = targetPrice;
+	}
+	/**
+	 * @return the stockRating
+	 */
+	public String getStockRating() {
+		return stockRating;
+	}
+	/**
+	 * @param stockRating the stockRating to set
+	 */
+	public void setStockRating(String stockRating) {
+		this.stockRating = stockRating;
+	}
+	/**
+	 * @return the stockRatingWM
+	 */
+	public String getStockRatingWM() {
+		return stockRatingWM;
+	}
+	/**
+	 * @param stockRatingWM the stockRatingWM to set
+	 */
+	public void setStockRatingWM(String stockRatingWM) {
+		this.stockRatingWM = stockRatingWM;
+	}
+	/**
+	 * @return the stockRatingDate
+	 */
+	public String getStockRatingDate() {
+		return stockRatingDate;
+	}
+	/**
+	 * @param stockRatingDate the stockRatingDate to set
+	 */
+	public void setStockRatingDate(String stockRatingDate) {
+		this.stockRatingDate = stockRatingDate;
+	}
+	/**
+	 * @return the highlight
+	 */
+	public String getHighlight() {
+		return highlight;
+	}
+	/**
+	 * @param highlight the highlight to set
+	 */
+	public void setHighlight(String highlight) {
+		this.highlight = highlight;
+	}
+	/**
+	 * @return the investmentRisk
+	 */
+	public String getInvestmentRisk() {
+		return investmentRisk;
+	}
+	/**
+	 * @param investmentRisk the investmentRisk to set
+	 */
+	public void setInvestmentRisk(String investmentRisk) {
+		this.investmentRisk = investmentRisk;
+	}
+	/**
+	 * @return the businessSummary
+	 */
+	public String getBusinessSummary() {
+		return businessSummary;
+	}
+	/**
+	 * @param businessSummary the businessSummary to set
+	 */
+	public void setBusinessSummary(String businessSummary) {
+		this.businessSummary = businessSummary;
+	}
+	/**
+	 * @return the subIndustryOutlook
+	 */
+	public String getSubIndustryOutlook() {
+		return subIndustryOutlook;
+	}
+	/**
+	 * @param subIndustryOutlook the subIndustryOutlook to set
+	 */
+	public void setSubIndustryOutlook(String subIndustryOutlook) {
+		this.subIndustryOutlook = subIndustryOutlook;
+	}
+	/**
+	 * @return the analystNote
+	 */
+	public String getAnalystNote() {
+		return analystNote;
+	}
+	/**
+	 * @param analystNote the analystNote to set
+	 */
+	public void setAnalystNote(String analystNote) {
+		this.analystNote = analystNote;
+	}
+	
+}
